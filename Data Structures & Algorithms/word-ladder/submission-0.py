7d6c1268a7287e4fffb55ee2f317c07ade6ae7ea
cindie <cindie060806@gmail.com>
2026-09-26 21:51:56 +0800

    Add: word-ladder - submission-0

---
+++ Data Structures & Algorithms/word-ladder/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def stringDiff(self, str_a, str_b):
+        cnt = 0
+        for i in range(len(str_a)):
+            if str_a[i] == str_b[i]:
+                continue
+            else:
+                cnt += 1
+        return cnt
+
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        q = deque([beginWord])
+        visit = set()
+        seq = 1
+        while q:
+            for _ in range(len(q)):
+                word = q.popleft()
+                visit.add(word)
+                if word == endWord:
+                    return seq
+                for w in wordList:
+                    if w not in visit and self.stringDiff(w, word) == 1:
+                        q.append(w)
+            seq += 1
+        return 0
